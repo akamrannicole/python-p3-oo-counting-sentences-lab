f788afdf4497c76aeecd17bc09ae2e9aba01073b
user.name <akamrannicole@gmail.com>
2024-06-07 14:45:58 +0300

    done

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,30 @@
-#!/usr/bin/env python3
+# lib/count_sentences.py
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self._value = value
+    
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, new_value):
+        if not isinstance(new_value, str):
+            print("The value must be a string.")
+        else:
+            self._value = new_value
+    
+    def is_sentence(self):
+        return self._value.endswith(".")
+    
+    def is_question(self):
+        return self._value.endswith("?")
+    
+    def is_exclamation(self):
+        return self._value.endswith("!")
+    
+    def count_sentences(self):
+        # Split by sentence-ending punctuation marks followed by a space or end of string.
+        sentences = [s for s in self._value.split() if s.endswith(('.', '!', '?'))]
+        return len(sentences)
